d7e870629f7df9469c21596588c4868a529958d5
Myroslav13 <myroslavshchuruk@gmail.com>
2025-08-24 17:59:44 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+interface Country {
+  cca3: string;
+  name: {
+    common: string;
+  };
+  population: number;
+  flags: {
+    svg: string;
+  };
+}
+
+function App() {
+  
+
+  return (
+    
+  )
+}
+
+export default App;
